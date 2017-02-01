a0bde1f416d3ac1ca70a88f5ebb1364686d45f56
ADL89 <avilandau1989@gmail.com>
2017-02-01 23:06:31 +0200

    Delete JNiFo.js

--- JNiFo/JNiFo/JNiFo.js
+++
@@ -1,8 +0,0 @@
-//IE Fix - allow copying from Disabeld fields
-function allowToCopyFromDisableds() {
-    i = NWF$('[disabled]');
-    NWF$(i).each(function () {
-        NWF$(this).removeProp("disabled");
-        NWF$(this).prop("readonly", true);
-    })
-}
